3c31b769677eea64fbe9f91d9b4dd77792187e67
Mark Lee <mark@protopie.io>
2018-01-11 01:23:15 +0900

    초기 설정 완료

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import './style.css';
+
+const App = () => <h1>Hello, parcel!</h1>;
+
+ReactDOM.render(<App />, document.querySelector('#root'));
